f3e73b9810f7c8ec9aa62d1a2184c996bf35a0b4
Kashfa Kulsoom <kashfakulsoom632@gmail.com>
2026-07-02 02:26:41 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "@/router";
+
+import vue3GoogleLogin from "vue3-google-login";
+
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+app.use(vue3GoogleLogin, {
+  clientId: "753912888585-b93071ko6q8ffcen7srv1iv5cid292di.apps.googleusercontent.com",
+});
+
+app.mount("#app");
